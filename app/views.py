72889a45bed2c9024ee6c8caffdd2a81309fcb09
Anastasia Stepanova <stepanovawm@gmail.com>
2024-11-10 19:32:28 +0300

    Something?..

--- app/views.py
+++ app/views.py
@@ -1,25 +1,61 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 QUESTIONS = [
     {
         'title': f'Some question! {i}',
         'id': i,
-        'text': f'Some text! {i}'
-    } for i in range(1, 30)
+        'text': f'Question text {i}'
+    } for i in range(15)
+]
+
+HOT_QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Hot question! {i}',
+        'text': f'Question text {i}'
+    } for i in range(1, 15)
 ]
 
 ANSWERS = [
     {
-        'title': f'Some answer! {i}',
         'id': i,
-        'text': f'Some text! {i}'
-    } for i in range(1, 20)
+        'title': f'Some answer! {i}',
+        'text': f'Answer text {i}'
+    } for i in range(1, 15)
 ]
 
-
 def index(request):
-    return render(request, 'index.html', {'questions': QUESTIONS})
+    paginator = Paginator(QUESTIONS, 5)
+    page_num = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_num).object_list
+    return render(request, 'index.html',
+                  {'questions': QUESTIONS, 'page_obj_list': page_obj, 'page_obj': page_obj})
 
 
-def question(request):
-    return render(request, 'question.html', {'answers': ANSWERS})
+def question(request, question_id):
+    return render(request, 'one_question.html', {'answers': ANSWERS, 'item': QUESTIONS[question_id]})
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def tag(request):
+    return render(request, 'tag.html', {'questions': QUESTIONS})
+
+
+def hot(request):
+    return render(request, 'hot.html', {'hot_questions': HOT_QUESTIONS})
--- askme_stepanova/urls.py
+++ askme_stepanova/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
+
 from app import views
 
 urlpatterns = [
-    path('', views.question, name='question')
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('settings/', views.settings, name='settings'),
+    path('ask/', views.ask, name='ask'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('tag/blablabla/', views.tag, name='tag'),
 ]
