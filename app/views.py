ddad72806d0c62afc0bd5d41928b7bb0268db034
Anastasia Stepanova <stepanovawm@gmail.com>
2024-11-10 23:37:36 +0300

    Finished

--- app/views.py
+++ app/views.py
@@ -1,43 +1,79 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 QUESTIONS = [
     {
-        'title': f'Some question! {i}',
         'id': i,
-        'text': f'Question text {i}'
-    } for i in range(15)
+        'title': f'Some question №{i}',
+        'text': f'Question text №{i}'
+    } for i in range(1, 30)
 ]
 
 HOT_QUESTIONS = [
     {
         'id': i,
-        'title': f'Hot question! {i}',
-        'text': f'Question text {i}'
-    } for i in range(15)
+        'title': f'Hot question №{i}',
+        'text': f'Hot question text №{i}'
+    } for i in range(1, 30)
 ]
 
 ANSWERS = [
     {
         'id': i,
-        'title': f'Some answer! {i}',
-        'text': f'Answer text {i}'
-    } for i in range(15)
+        'title': f'Answer №{i}',
+        'text': f'Answer text №{i}'
+    } for i in range(1, 15)
 ]
 
+default_page = 1
+items_per_page = 5
+
 def index(request):
-    return render(request, 'index.html', {'questions': QUESTIONS})
+    page_num = request.GET.get('page', default_page)
+    paginator = Paginator(QUESTIONS, items_per_page)
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(default_page)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'page_obj': page_obj})
 
 
 def question(request, question_id):
-    return render(request, 'one_question.html', {'answers': ANSWERS, 'item': QUESTIONS[question_id]})
+    page_num = request.GET.get('page', default_page)
+    paginator = Paginator(ANSWERS, items_per_page)
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(default_page)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request, 'one_question.html', {'item': QUESTIONS[question_id - 1], 'page_obj': page_obj})
 
 
 def tag(request):
-    return render(request, 'tag.html', {'questions': QUESTIONS})
+    page_num = request.GET.get('page', default_page)
+    paginator = Paginator(QUESTIONS, items_per_page)
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(default_page)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request, 'tag.html', {'page_obj': page_obj})
 
 
 def hot(request):
-    return render(request, 'hot.html', {'hot_questions': HOT_QUESTIONS})
+    page_num = request.GET.get('page', default_page)
+    paginator = Paginator(HOT_QUESTIONS, items_per_page)
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_obj = paginator.page(default_page)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return render(request, 'hot.html', {'page_obj': page_obj})
 
 
 def settings(request):
